3fba6f1152bab05b58edf547f94ae92624f8adb9
Erik Osmond <erikosmond@gmail.com>
2019-04-04 16:25:10 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSsmProvider.MixProject do
   def project do
     [
       app: :aws_ssm_provider,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       name: "AwsSsmProvider",
       description: "A configuration provider for Distillery which handles AWS SSM parameters",
