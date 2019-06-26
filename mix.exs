4475ab6c549a0b2147fe44d016d2c24718f71743
James Lavin <james@caredox.com>
2019-06-26 10:25:35 -0400

    bump minor version (0.2.1 => 0.3.0); add Distillery version compatibility explanation to README installation instructions; bump Elixir requirement to 1.8

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule AwsSsmProvider.MixProject do
   def project do
     [
       app: :aws_ssm_provider,
-      version: "0.2.1",
-      elixir: "~> 1.7",
+      version: "0.3.0",
+      elixir: "~> 1.8",
       name: "AwsSsmProvider",
       description: "A configuration provider for Distillery which handles AWS SSM parameters",
       package: package(),
